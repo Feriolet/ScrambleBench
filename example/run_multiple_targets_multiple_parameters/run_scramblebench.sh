4cd3bf84cacbd51d89fdada7533a03c60f93cede
Feriolet <veincent.yap@gmail.com>
2026-05-18 01:40:53 +0800

    add data analysis to script

--- example/run_multiple_targets_multiple_parameters/run_scramblebench.sh
+++ example/run_multiple_targets_multiple_parameters/run_scramblebench.sh
@@ -7,3 +7,4 @@ python $SCRAMBLEBENCH_SCRIPT_DIR/p3_prepare_molecule.py -i output/yaml_list.txt
 python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_diversity.py -i output/yaml_list.txt
 python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_genbench3d.py -i output/yaml_list.txt
 python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_redocking.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p5_collect_data_analysis.py -i output/yaml_list.txt
--- example/run_without_protein_input/run_scramblebench.sh
+++ example/run_without_protein_input/run_scramblebench.sh
@@ -4,3 +4,4 @@ python $SCRAMBLEBENCH_SCRIPT_DIR/p2_execute_generation.py -i Protein_5HT2A/confi
 python $SCRAMBLEBENCH_SCRIPT_DIR/p3_prepare_molecule.py -i Protein_5HT2A/config_test_run_clean_config.yml
 python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_diversity.py -i Protein_5HT2A/config_test_run_clean_config.yml
 python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_genbench3d.py -i Protein_5HT2A/config_test_run_clean_config.yml
+python $SCRAMBLEBENCH_SCRIPT_DIR/p5_collect_data_analysis.py -i Protein_5HT2A/config_test_run_clean_config.yml
