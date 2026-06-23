5d048cd230ffed5afbee72d073e28c318991ebb7
veincent <veincent.yap@gmail.com>
2026-06-23 17:49:48 +0800

    add new output file

---
+++ example/run_multiple_targets_multiple_parameters/run_scramblebench.sh
@@ -0,0 +1,12 @@
+set -e
+SCRAMBLEBENCH_SCRIPT_DIR='../../src/scramblebench/script'
+
+bash ./prepare_config.sh
+
+python $SCRAMBLEBENCH_SCRIPT_DIR/p2_execute_generation.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p3_prepare_molecule.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_diversity.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_genbench3d.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p4_analyse_redocking.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p5_collect_data_analysis.py -i output/yaml_list.txt
+python $SCRAMBLEBENCH_SCRIPT_DIR/p6_generate_report.py -i output/yaml_list.txt
